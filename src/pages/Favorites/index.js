382658b075b0a5f5a02d33cdd710c734388919f7
Felype-Henrique <felypehenrique25@gmail.com>
2022-05-09 01:42:15 -0300

    Favorites

--- src/pages/Favorites/index.js
+++ src/pages/Favorites/index.js
@@ -1,10 +1,30 @@
 import React from "react";
-import {} from './styles'
+import {Description, Group, Header, Star, Stars, Title, TitleGroup} from './styles'
+import SearchBar from '../../components/searchBar/index'
+import {BiStar} from "react-icons/bi";
 
 const Favorites = () => {
+  
+  const favo = localStorage.getItem('favo')
+  const repos = JSON.parse(favo);
   return (
-    <h1>Favorites</h1>
-  )
-}
+    <>
+      <Header>
+        <Title>REPOSITÓRIOS FAVORITOS</Title>
+        <SearchBar/>
+      </Header>
+      {(repos).map(repo => (
+      <Group key={repo.id}>
+          <TitleGroup>{repo.name}</TitleGroup>
+          <Description>{repo.description}</Description>
+          <Stars>
+          <BiStar size={24} />
+          <Star>{repo.stargazers_count}</Star>
+          </Stars>
+      </Group>
+       ) )}
+    </>
+  );
+};
 
 export default Favorites;
--- src/pages/Favorites/styles.js
+++ src/pages/Favorites/styles.js
@@ -1,1 +1,48 @@
 import styled from "styled-components";
+
+export const Header = styled.div`
+  display: flex;
+  margin: 20px;
+  justify-content: space-between;
+`;
+
+export const Title = styled.h1`
+font-family: 'Roboto Mono';
+font-weight: 400;
+font-size: 42px;
+line-height: 55px;
+color: #000000;
+`;
+
+export const Group = styled.div`
+  margin: 50px 20px 20px 20px;
+  width: 80%;
+`;
+
+export const Stars = styled.div`
+  display: flex;
+`;
+
+export const TitleGroup = styled.h1`
+font-family: 'Rubik';
+font-weight: 300;
+font-size: 36px;
+line-height: 43px;
+color: #000000;
+`;
+
+export const Description = styled.h2`
+font-family: 'Rubik';
+font-weight: 300;
+font-size: 24px;
+line-height: 28px;
+color: #757575;
+`;
+
+export const Star = styled.h3`
+font-family: 'Rubik';
+font-weight: 300;
+font-size: 20px;
+line-height: 24px;
+color: #757575;
+`;
