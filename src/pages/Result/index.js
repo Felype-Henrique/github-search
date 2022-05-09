c4c3fe8ed948c31afdcf2de04f398a792f8c70d7
Felype-Henrique <felypehenrique25@gmail.com>
2022-05-08 23:45:57 -0300

    Result

--- src/pages/Result/index.js
+++ src/pages/Result/index.js
@@ -1,10 +1,109 @@
-import React from "react";
-import {} from './styles'
+import React, { useContext, useEffect, useState } from "react";
+import {
+  Title,
+  SubTitle,
+  Logo,
+  Header,
+  Columns,
+  SideBar,
+  Name,
+  UserName,
+  Organization,
+  Text,
+  Main,
+  Group,
+  TitleRepo,
+  Desc,
+  Stars,
+  Repo,
+  TitleFav,
+  TitleRep,
+  Button,
+} from "./styles";
+import SearchBar from "../../components/searchBar/index";
+import {
+  BiBriefcaseAlt2,
+  BiLocationPlus,
+  BiStar,
+  BiFolderOpen,
+} from "react-icons/bi";
+import { IoPeopleSharp } from "react-icons/io5";
+
+import { context } from "../../context";
 
 const Result = () => {
+  const ctx = useContext(context);
+
+  const [fav, setFav] = useState([]);
+
+  useEffect(() => {
+    localStorage.setItem("favo", JSON.stringify(fav));
+  }, [fav]);
+
   return (
-    <h1>Result</h1>
-  )
-}
+    <>
+      <Header>
+        <Logo>
+          <Title>GitHub</Title>
+          <SubTitle>Search</SubTitle>
+        </Logo>
+        <SearchBar />
+      </Header>
+      <Columns>
+        <SideBar>
+          <img src={ctx.userData?.avatar_url} alt={ctx.userData?.login} />
+          <Name>{ctx.userData?.name}</Name>
+          <UserName>{ctx.userData?.login}</UserName>
+          <Organization>
+            <BiBriefcaseAlt2 size={24} />
+            <Text>{ctx.userData?.company}</Text>
+          </Organization>
+          <Organization>
+            <BiLocationPlus size={24} />
+            <Text>{ctx.userData?.location}</Text>
+          </Organization>
+          <Organization>
+            <BiStar size={24} />
+            <Text>0</Text>
+          </Organization>
+          <Organization>
+            <BiFolderOpen size={24} />
+            <Text>{ctx.userData?.public_repos}</Text>
+          </Organization>
+          <Organization>
+            <IoPeopleSharp size={24} />
+            <Text>{ctx.userData?.followers}</Text>
+          </Organization>
+        </SideBar>
+        <Main>
+          {ctx.repos.map((repo) => (
+            <Group key={repo.id}>
+              <TitleRep>
+                <TitleRepo>{repo.name}</TitleRepo>
+                <Button onClick={() => setFav([...fav, repo])}>
+                  <BiStar size={24} />
+                </Button>
+              </TitleRep>
+              <Desc>{repo.description}</Desc>
+              <Stars>
+                <BiStar size={24} />
+                <Text>{repo.stargazers_count}</Text>
+                <button onClick={() => console.log(fav)}>console</button>
+              </Stars>
+            </Group>
+          ))}
+        </Main>
+        <SideBar>
+          <Repo>
+            <TitleFav>Repositórios favoritados</TitleFav>
+          </Repo>
+          <Repo>
+            <TitleFav>Repositórios Não favoritados</TitleFav>
+          </Repo>
+        </SideBar>
+      </Columns>
+    </>
+  );
+};
 
 export default Result;
--- src/pages/Result/styles.js
+++ src/pages/Result/styles.js
@@ -1,1 +1,124 @@
 import styled from "styled-components";
+
+export const Title = styled.h1`
+  font-weight: 700;
+  font: 42px Roboto Mono;
+  color: #000000;
+  margin-right: 10px;
+`;
+
+export const SubTitle = styled.h1`
+  font-weight: 300;
+  font: 42px Rubik;
+  color: #000000;
+  margin-top: 8px;
+`;
+
+export const Logo = styled.div`
+  display: flex;
+`;
+
+export const Header = styled.div`
+  display: flex;
+  margin: 20px;
+  margin-right: 260px;
+  justify-content: space-between;
+`;
+
+export const Columns = styled.div`
+  display: grid;
+
+  grid-template-columns: 20% 60% 20%;
+`;
+
+export const SideBar = styled.div`
+
+  margin: 20px;
+   img{
+     height: 300px;
+     width: 300px;
+   }
+`;
+
+export const Name = styled.h1`
+  font-family: 'Rubik';
+  font-weight: 300;
+  font-size: 30px;
+`;
+
+export const UserName = styled.h2`
+  font-family: 'Rubik';
+  font-weight: 300;
+  font-size: 24px;
+  line-height: 28px;
+  color: #757575;
+`;
+
+export const Organization = styled.div`
+   display: flex;
+   margin-top: 5px;
+`;
+
+export const Text = styled.h3`
+margin-left: 10px;
+font-family: 'Rubik';
+font-weight: 300;
+font-size: 20px;
+line-height: 24px;
+color: #757575;
+`;
+
+export const Main = styled.div`
+   margin: 90px;
+`;
+
+export const Group = styled.div`
+  position: relative;
+  margin-bottom: 80px;
+  height: 105px;
+`;
+
+export const TitleRepo = styled.h1`
+  display: flex;
+  font-family: 'Rubik';
+  font-weight: 300;
+  font-size: 36px;
+  line-height: 43px;
+  color: #000000;
+`;
+
+export const TitleRep = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const Button = styled.button`
+  border: none;
+`;
+
+export const Desc = styled.h2`
+  font-family: 'Rubik';
+  font-weight: 300;
+  font-size: 24px;
+  line-height: 28px;
+
+  color: #757575;
+`;
+
+export const Stars = styled.div`
+   display: flex;
+`;
+
+export const Repo = styled.div`
+   display: flex;
+   margin-top: 100px;
+   width: 190px;
+`;
+
+export const TitleFav = styled.h1`
+  font-family: 'Rubik';
+  font-weight: 300;
+  font-size: 20px;
+  line-height: 24px;
+  color: #000000;
+`;
